d921f6efcd5fb1c9f8008f679b55f69a7d5099c1
Admin <admin@Admins-Mac.local>
2019-02-20 20:46:15 +0700

    first commit

---
+++ AppNews/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  AppNews
+//
+//  Created by Admin on 2/19/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
---
+++ AppNews/SignUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewController.swift
+//  AppNews
+//
+//  Created by Admin on 2/19/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
