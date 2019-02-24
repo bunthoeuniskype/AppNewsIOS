36f2e0cdbe9c8b6887445581aaa2a1db090b9985
Admin <admin@Admins-Mac.local>
2019-02-24 14:36:38 +0700

    auto-layout-container

---
+++ AppNews/PostTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  PostTableViewCell.swift
+//  AppNews
+//
+//  Created by Admin on 2/24/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet weak var postTextLabel: UILabel!    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        profileImageView.layer.cornerRadius = profileImageView.bounds.height / 2
+        profileImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func set(post:Post) {
+        usernameLabel.text = post.author
+        postTextLabel.text = post.text
+    }
+    
+}
